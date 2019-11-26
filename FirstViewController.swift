574ae510d73855c4ff090dfe862dfc940b2b461a
mp-38 <maxpisarik@yahoo.com>
2019-11-26 17:29:47 -0500

    project start

---
+++ FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  DrugBook
+//
+//  Created by Maksim Pisaryk on 11/26/19.
+//  Copyright © 2019 cecs. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
